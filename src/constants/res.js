a7333c44361215346712a7e33f1a7ed11f055734
klin <klin0816@gmail.com>
2022-05-09 15:01:28 +0800

    chore(src/constants/res.js): refine line thickness and color

--- src/constants/res.js
+++ src/constants/res.js
@@ -1,3 +1,3 @@
 export const LAYER_COLOR = ["red", "green", "blue"];
-export const LINE_COLOR = "green";
-export const LINE_WIDTH = 5;
+export const LINE_COLOR = "black";
+export const LINE_WIDTH = 2;
